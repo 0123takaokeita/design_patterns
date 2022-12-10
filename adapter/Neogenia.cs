a9fc02839b1548244748563a34b6fa41ab75d439
Takao <k.takao@neogenia.co.jp>
2022-12-11 02:00:38 +0900

    feat: アダプターパターン委譲

---
+++ adapter/Neogenia.cs
@@ -0,0 +1,53 @@
+
+//// 委譲パターン
+//namespace Neogenia
+//{
+//    /// <summary>
+//    /// TeachLeadのインターフェース
+//    /// </summary>
+//    interface ITechLead
+//    {
+//        public void recreate();
+//    }
+
+//    /// <summary>
+//    /// 社員に指示を出すクラス
+//    /// </summary>
+//    class Neogneia
+//    {
+
+//        static void Main(string[] args)
+//        {
+//            ITechLead techLead = new TechLead();
+//            techLead.recreate();
+//            Console.ReadLine();
+//        }
+//    }
+
+//    /// <summary>
+//    /// テックリードポジションのクラス
+//    /// </summary>
+//    class TechLead : ITechLead
+//    {
+//        private Programmer _programmer = new Programmer();
+
+//        public void recreate()
+//        {
+//            //Console.WriteLine("TechLead が作り直します。");
+//            //呼び出し側からは見えないが、より効率の良い関数に切り替える。
+//            _programmer.refactor();
+//        }
+//    }
+
+//    /// <summary>
+//    /// プログラマ
+//    /// </summary>
+//    class Programmer
+//    {
+//        public void refactor()
+//        {
+//            Console.WriteLine("Programmer がリファクタリングします。");
+//        }
+//    }
+
+//}
