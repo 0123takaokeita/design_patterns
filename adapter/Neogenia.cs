a8459bc3d3c7fe2b38f28c5215e100fdb014a43b
Takao <k.takao@neogenia.co.jp>
2022-12-12 10:22:51 +0900

    fix: 抽象クラスがある時のパターン

--- adapter/Neogenia.cs
+++ adapter/Neogenia.cs
@@ -1,53 +1,51 @@
+// 継承パターン
+namespace Neogenia
+{
+    /// <summary>
+    /// TeachLeadのインターフェース
+    /// </summary>
+    public abstract class AbstractTechLead
+    {
+        public abstract void recreate();
+    }
 
-//// 委譲パターン
-//namespace Neogenia
-//{
-//    /// <summary>
-//    /// TeachLeadのインターフェース
-//    /// </summary>
-//    interface ITechLead
-//    {
-//        public void recreate();
-//    }
+    /// <summary>
+    /// 社員に指示を出すクラス
+    /// </summary>
+    class Neogneia
+    {
 
-//    /// <summary>
-//    /// 社員に指示を出すクラス
-//    /// </summary>
-//    class Neogneia
-//    {
+        static void Main(string[] args)
+        {
+            TechLead techLead = new TechLead();
+            techLead.recreate();
+            Console.ReadLine();
+        }
+    }
 
-//        static void Main(string[] args)
-//        {
-//            ITechLead techLead = new TechLead();
-//            techLead.recreate();
-//            Console.ReadLine();
-//        }
-//    }
+    /// <summary>
+    /// テックリードポジションのクラス
+    /// </summary>
+    class TechLead : AbstractTechLead
+    {
+        private Programmer _programmer = new Programmer();
 
-//    /// <summary>
-//    /// テックリードポジションのクラス
-//    /// </summary>
-//    class TechLead : ITechLead
-//    {
-//        private Programmer _programmer = new Programmer();
+        public override void recreate()
+        {
+            //Console.WriteLine("TechLead が作り直します。");
+            //呼び出し側からは見えないが、より効率の良い関数に切り替える。
+            _programmer.refactor();
+        }
+    }
 
-//        public void recreate()
-//        {
-//            //Console.WriteLine("TechLead が作り直します。");
-//            //呼び出し側からは見えないが、より効率の良い関数に切り替える。
-//            _programmer.refactor();
-//        }
-//    }
-
-//    /// <summary>
-//    /// プログラマ
-//    /// </summary>
-//    class Programmer
-//    {
-//        public void refactor()
-//        {
-//            Console.WriteLine("Programmer がリファクタリングします。");
-//        }
-//    }
-
-//}
+    /// <summary>
+    /// プログラマ
+    /// </summary>
+    class Programmer
+    {
+        public void refactor()
+        {
+            Console.WriteLine("Programmer がリファクタリングします。");
+        }
+    }
+}
