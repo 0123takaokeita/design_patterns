bd0ee5d3fdce816757a39b9503ff7a235b797cb9
Takao <k.takao@neogenia.co.jp>
2022-12-26 10:25:16 +0900

    feat: Factory Class

--- FactoryMethod/Factory.cs
+++ FactoryMethod/Factory.cs
@@ -5,3 +5,31 @@ public abstract class Product
     public abstract void use();
 }
 
+public abstract class Factory
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="owner"></param>
+    /// <returns></returns>
+    protected abstract Product createProduct(string owner);
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="product"></param>
+    protected abstract void registerProduct(Product product);
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="owner"></param>
+    /// <returns></returns>
+    public Product create(string owner)
+    {
+        var product = createProduct(owner);
+        registerProduct(product);
+        return product;
+    }
+}
+
