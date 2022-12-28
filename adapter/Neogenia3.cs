c2ff94ca26e5b24da92e8158efecdad0049c5dc7
Takao <k.takao@neogenia.co.jp>
2022-12-28 09:37:44 +0900

    fix: addapter でmain重複のエラーがうるさいのでコメントアウトした

--- adapter/Neogenia3.cs
+++ adapter/Neogenia3.cs
@@ -1,58 +1,58 @@
-﻿//これは？
-namespace Neogenia3
-{
-    /// <summary>
-    /// TeachLeadのインターフェース
-    /// </summary>
-    public interface ITechLead
-    {
-        public void recreate();
-    }
-
-    /// <summary>
-    /// 社員に指示を出すクラス
-    /// </summary>
-    class Neogneia
-    {
-        /// <summary>
-        /// ルート
-        /// </summary>
-        /// <param name="args"></param>
-        static void Main(string[] args)
-        {
-            ITechLead techLead = new NewTechLead();
-            techLead.recreate();
-            Console.ReadLine();
-        }
-    }
-
-    /// <summary>
-    /// プログラマ
-    /// </summary>
-    class Programmer
-    {
-        /// <summary>
-        /// リファクタリングを行う
-        /// </summary>
-        public void refactor()
-        {
-            Console.WriteLine("Programmer がリファクタリングします。");
-        }
-    }
-
-    /// <summary>
-    /// 新しいテックリード
-    /// </summary>
-    class NewTechLead : ITechLead
-    {
-        Programmer programmer = new Programmer();
-
-        /// <summary>
-        /// 作り直します。
-        /// </summary>
-        public void recreate()
-        {
-            programmer.refactor();
-        }
-    }
-}
+﻿// //これは？
+// namespace Neogenia3
+// {
+//     /// <summary>
+//     /// TeachLeadのインターフェース
+//     /// </summary>
+//     public interface ITechLead
+//     {
+//         public void recreate();
+//     }
+//
+//     /// <summary>
+//     /// 社員に指示を出すクラス
+//     /// </summary>
+//     class Neogneia
+//     {
+//         /// <summary>
+//         /// ルート
+//         /// </summary>
+//         /// <param name="args"></param>
+//         static void Main(string[] args)
+//         {
+//             ITechLead techLead = new NewTechLead();
+//             techLead.recreate();
+//             Console.ReadLine();
+//         }
+//     }
+//
+//     /// <summary>
+//     /// プログラマ
+//     /// </summary>
+//     class Programmer
+//     {
+//         /// <summary>
+//         /// リファクタリングを行う
+//         /// </summary>
+//         public void refactor()
+//         {
+//             Console.WriteLine("Programmer がリファクタリングします。");
+//         }
+//     }
+//
+//     /// <summary>
+//     /// 新しいテックリード
+//     /// </summary>
+//     class NewTechLead : ITechLead
+//     {
+//         Programmer programmer = new Programmer();
+//
+//         /// <summary>
+//         /// 作り直します。
+//         /// </summary>
+//         public void recreate()
+//         {
+//             programmer.refactor();
+//         }
+//     }
+// }
