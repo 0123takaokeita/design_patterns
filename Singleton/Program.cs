877607d24a06264b8e46e411d93734243b837846
Takao <k.takao@neogenia.co.jp>
2023-01-01 16:30:28 +0900

    feat: singleton プログラム作成

--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -1,3 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace singleton;
 
-Console.WriteLine("Hello, World!");
+public class Singleton
+{
+    // private でインスタンスを作っているのがポイント
+    private static Singleton instance = new Singleton();
+    
+    // このコンストラクターは外から呼び出せないため インスタンスを１つに統一する事ができる。
+    private Singleton()
+    {
+        Console.WriteLine("インスタンス生成に成功しました");
+    }
+
+    public static Singleton GetInstance()
+    {
+        return instance;
+    }
+}
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var obj = Singleton.GetInstance();
+        var obj2 = Singleton.GetInstance();
+        
+        Console.WriteLine("### START ###");
+        Console.WriteLine(obj == obj2 ? "objを同じインスタンを参照しています。" : "objは異なるインスタンスを参照しています。 設計を見直しましょう");
+        Console.WriteLine("### END ###");
+    }
+}
