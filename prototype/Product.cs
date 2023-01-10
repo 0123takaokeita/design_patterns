0d1226c3c895b0e2c3acfb903b2ed34f6559c3e6
Takao <k.takao@neogenia.co.jp>
2023-01-10 09:55:52 +0900

    feat: classDiagram雛形作成

--- prototype/Product.cs
+++ prototype/Product.cs
@@ -1,7 +1,26 @@
 namespace prototype;
 
-public interface Product
+public interface Product : ICloneable
 {
     public abstract void use(string s);
-    public abstract Product cleateClone();
+    public abstract Product createClone();
+
+    public object Clone()
+    {
+        return MemberwiseClone();
+    } 
+    
+    // 仮に createClone が全く同じ実装なのだとしたら interface に書いておくかな。
+    private Product createClone()
+    {
+        Product p = null;
+        try
+        {
+            return p = (Product)Clone();
+        }
+        catch (CloneNotSupportedException e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
--- prototype/Program.cs
+++ prototype/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace prototype;
 
-Console.WriteLine("Hello, World!");
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var manager = new Manager();
+        var underlinePen = new UnderlinePen();
+        var messageBox= new MessageBox('*');
+        var slashBox = new MessageBox('/');
+        manager.register("strong message", underlinePen);
+        manager.register("warning box", messageBox);
+        manager.register("slash box",slashBox);
+
+        // create では createClone をおこなっているので実際の名前は知らなくても良い。
+        // 今回は文字列がkeyになっているがこれは一意になっていれば設計上は変更しても良さそう。
+        var p1 = manager.create("strong message");
+        p1.use("Hello world");
+        var p2 = manager.create("warning box");
+        p2.use("Hello world");
+        var p3 = manager.create("slash box");
+        p3.use("Hello world");
+    } 
+}
