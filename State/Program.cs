88db03d7992a38de4b961675b9166d4c1567ddda
Takao <k.takao@neogenia.co.jp>
2023-04-23 12:07:36 +0900

    feat: 記事の状態変化を表現するサンプルコードの作成

--- State/Program.cs
+++ State/Program.cs
@@ -1,3 +1,196 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace State;
 
-Console.WriteLine("Hello, World!");
+// State クラス
+// 記事の状態を表現するインターフェース
+interface IArticleState
+{
+    void Show(Article article);
+    void Publish(Article article);
+    void Hide(Article article);
+    void Delete(Article article);
+}
+
+// Context クラス
+// 記事を表現するクラス
+class Article
+{
+    private IArticleState state;
+    public string title { get; set; }
+
+    public Article(string title)
+    {
+        state = new DraftState();
+        this.title = title;
+    }
+
+    public void Show()
+    {
+        state.Show(this);
+    }
+
+    public void Hide()
+    {
+        state.Hide(this);
+    }
+
+    public void Delete()
+    {
+        state.Delete(this);
+    }
+
+    public void Publish()
+    {
+        state.Publish(this);
+    }
+
+    // 状態を変更する
+    // このメソッドがないと、状態を変更することができない。
+    public void ChangeState(IArticleState state)
+    {
+        this.state = state;
+    }
+}
+
+// 公開状態を表現する ConcreteState
+class PublishedState : IArticleState
+{
+    public string stateName = "公開";
+    public void Show(Article article)
+    {
+        Console.WriteLine($"title: {article.title}は{stateName}されています。");
+    }
+
+    public void Publish(Article article)
+    {
+        Console.WriteLine($"title: {article.title}はすでに{stateName}済みです。");
+    }
+
+    public void Hide(Article article)
+    {
+        DraftState state = new();
+        article.ChangeState(state);
+        Console.WriteLine($"title: {article.title}を{state.stateName}にしました。");
+    }
+
+    public void Delete(Article article)
+    {
+        article.ChangeState(new DeletedState());
+        Console.WriteLine($"title: {article.title}を削除しました。");
+    }
+}
+
+// 下書き状態を表現する ConcreteState
+class DraftState : IArticleState
+{
+    public string stateName = "非公開";
+
+    // 状態を表示する。
+    public void Show(Article article)
+    {
+        Console.WriteLine($"title: {article.title} は {stateName}です");
+    }
+
+    // 公開する
+    public void Publish(Article article)
+    {
+        PublishedState state = new();
+        article.ChangeState(state);
+        Console.WriteLine($"title: {article.title}を{state.stateName}しました。");
+    }
+
+    // 状態を表示する。
+    public void Hide(Article article)
+    {
+        Console.WriteLine($"title: {article.title} はすでに {stateName} です。");
+    }
+
+    // 削除済みにする
+    public void Delete(Article article)
+    {
+        DeletedState state = new();
+        article.ChangeState(state);
+        Console.WriteLine($"title: {article.title}を削除しました。");
+    }
+}
+
+// 削除済み状態を表現する ConcreteState
+class DeletedState : IArticleState
+{
+    public string stateName = "削除済み";
+
+    public void Show(Article article)
+    {
+        Console.WriteLine($"{article.title}は削除されています。");
+    }
+
+    // 公開する
+    public void Publish(Article article)
+    {
+        Console.WriteLine($"{stateName}の記事を公開することはできません。");
+    }
+
+    // 非公開にする
+    public void Hide(Article article)
+    {
+        Console.WriteLine($"{stateName}の記事を非公開にすることはできません。");
+    }
+
+    public void Delete(Article article)
+    {
+        Console.WriteLine($"{article.title}削除されています。");
+    }
+}
+
+
+// クライアント
+class Client
+{
+    static void Main(string[] args)
+    {
+        // 記事を作成しますか？ をYes/Noで聞く
+        // Yesの場合は記事のタイトルを入力してもらう
+        // Noの場合は終了する
+        Console.WriteLine("記事を作成しますか？");
+        Console.WriteLine("Yes: y, No: n");
+        string input = Console.ReadLine() ?? "";
+        if (input != "y") return;
+        separator();
+        // タイトルを入力してもらい、空の場合はもう一度入力してもらう。
+        while (true)
+        {
+            Console.WriteLine("タイトルを入力してください。");
+            input = Console.ReadLine() ?? "";
+            if (input != "") break;
+        }
+
+        Article article = new(input);
+        separator();
+
+        // 記事の状態を確認する。
+        Console.WriteLine("公開のチェック");
+        article.Show();
+        article.Hide();
+        article.Publish();
+        separator();
+
+        // 記事の状態を確認する。
+        Console.WriteLine("非公開のチェック");
+        article.Show();
+        article.Hide();
+        article.Delete();
+        separator();
+
+        // 記事の状態を確認する。
+        Console.WriteLine("削除済みのチェック");
+        article.Show();
+        article.Hide();
+        article.Publish();
+        article.Delete();
+        separator();
+    }
+
+    static void separator()
+    {
+        Console.WriteLine("====================================\n");
+    }
+}
