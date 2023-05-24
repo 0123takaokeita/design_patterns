846cad1557cc449b946229c5c9fc244d1b4939b3
KeitaTakao <keita.takao.1112@gmail.com>
2023-05-24 12:04:20 +0900

    fix: Text Editor の雛形作成

--- Command/Program.cs
+++ Command/Program.cs
@@ -1,2 +1,87 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// undo/redo を機能実装したテキスエディタ
+
+
+// Command 役
+interface ICommand
+{
+    void Execute();
+    void Undo();
+}
+
+// ConcreateCommand 役
+class TextEditCommand : ICommand
+{
+    private readonly TextEditor _editor;
+    private readonly string _text;
+    private string _previousText;
+
+    // コンストラクタ
+    public TextEditCommand(TextEditor editor, string text)
+    {
+        _editor = editor;
+        _text = text;
+        _previousText = "";
+    }
+
+    // 実行
+    public void Execute() { }
+
+    // 元に戻す
+    public void Undo() { }
+}
+
+// macro command
+class SnippetCommand : ICommand
+{
+    private readonly TextEditor _editor;
+    private readonly string _text;
+
+    // コンストラクタ
+    public SnippetCommand(TextEditor editor, string text) { }
+
+    // 実行
+    public void Execute() { }
+
+    // 元に戻す
+    public void Undo() { }
+}
+
+// Receiver 役
+// 命令を受け取る役
+// singletonにしないとだめ？
+class TextEditor
+{
+    // Undo Redo は１つの配列でできるのでは？
+    // undo methodは逆操作を実行する考え方もある。
+    // private index
+    private readonly Stack<ICommand> _undoStack = new Stack<ICommand>();
+    private readonly Stack<ICommand> _redoStack = new Stack<ICommand>();
+    private bool _isInsertMode = true;
+
+    public string Text { get; set; } = "";
+
+    public void HandleKey(char key)
+    {
+    }
+
+    public void ExecuteCommand(ICommand command)
+    {
+    }
+
+    public void Undo()
+    {
+    }
+
+    public void Redo()
+    {
+    }
+}
+
+// Client 役
+// Invoker 役
+class Program
+{
+    static void Main(string[] args)
+    {
+    }
+}
--- Command/README.md
+++ Command/README.md
@@ -1,22 +1,51 @@
 # 用途、ユースケースを自分の言葉で説明 (約1分)
+コマンドパターンとは履歴を管理し、それを実行するという形で実行インスタンスを管理する設計
+
+1. テキストエディタのundo/redo
+2. GUIボタンをコマンドにしてマクロ登録
+3. テストの自動化（実行順序を記録）
+4. 音楽プレイヤーのボタン操作とか？
 
 # サンプルの概要説明 (約1分)
-名前： 〜〜システム
+名前： テキストエディタのundo/redo
 
 # クラス図の説明 (約1分)
 本に書かれているクラスがどれに該当するか説明する。
-    classA --|> classB : Inheritance
-    classC --* classD : Composition
-    classE --o classF : Aggregation
-    classG --> classH : Association
-    classI -- classJ : Link(Solid)
-    classK ..> classL : Dependency
-    classM ..|> classN : Realization
-    classO .. classP : Link(Dashed)
+
+| original | text |
+| ---------| -----| 
+|ICommand| Command|
+|TextEditor | Receiver|
+|SnipetCommand |ConcreateCommand|
+|TextEditCommand | ConcreateCommand|
+|Main | Invoker, Client|
+
+
 ```mermaid
 classDiagram
-    class A { }
-    class B { }
+    class ICommand {
+
+        }
+    class TextEditCommand {
+
+        }
+    class SnipetCommand {
+
+        }
+    class TextEditor {
+
+        }
+    class Main {
+
+        }
+
+    ICommand <|-- TextEditCommand : Inheritance
+    ICommand <|-- SnipetCommand : Inheritance
+    TextEditCommand o-- TextEditor : Aggregation
+    SnipetCommand o-- TextEditor : Aggregation
+    TextEditor <-- Main : create
+    TextEditCommand <-- Main : create
+    SnipetCommand <-- Main : create
 ```
 
 # ソースコードの説明 (約1分)
